107269f553babdc32e6f88c2b63f4c902e5d9f38
laflamablanc <sean.laflam@gmail.com>
2020-08-27 14:58:28 -0400

    Cat Down Owner almost done

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,20 @@
 class Dog
+
+attr_accessor :owner, :mood
+attr_reader :name
+@@all = []
+def initialize(name, owner, mood="nervous")
+  @name = name
+  @owner = owner
+  @mood = mood
+  @@all<< self
+
+end
+
+def self.all
+  @@all
+end
+
+
   # code goes here
 end
